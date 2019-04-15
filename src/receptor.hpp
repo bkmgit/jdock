798ae4c0fbabdf00fedcc1d8ce656772b5cbc863
Jacky Lee <JackyLeeHongJian@Gmail.com>
2019-04-15 15:10:03 +0800

    Added a private section to receptor.hpp

--- src/receptor.hpp
+++ src/receptor.hpp
@@ -14,16 +14,12 @@ public:
 	//! Constructs a receptor by parsing a receptor file in pdbqt format.
 	explicit receptor(const path& p, const array<double, 3>& center, const array<double, 3>& size, const double granularity);
 
-	const array<double, 3> center; //!< Box center.
-	const array<double, 3> size; //!< 3D sizes of box.
 	const array<double, 3> corner0; //!< Box boundary corner with smallest values of all the 3 dimensions.
 	const array<double, 3> corner1; //!< Box boundary corner with largest values of all the 3 dimensions.
-	const double granularity; //!< 1D size of grids.
 	const double granularity_inverse; //!< 1 / granularity.
 	const array<size_t, 3> num_probes; //!< Number of probes.
-	const size_t num_probes_product; //!< Product of num_probes[0,1,2].
 	vector<atom> atoms; //!< Receptor atoms.
-	vector<vector<size_t>> p_offset; //!< Auxiliary precalculated constants to accelerate grid map creation.
+	const size_t num_probes_product; //!< Product of num_probes[0,1,2].
 	vector<vector<double>> maps; //!< Grid maps.
 
 	//! Returns true if a coordinate is within current half-open-half-close box, i.e. [corner0, corner1).
@@ -40,6 +36,12 @@ public:
 
 	//! Populates grid maps for certain atom types along X and Y dimensions for a given Z dimension value.
 	void populate(const vector<size_t>& xs, const size_t z, const scoring_function& sf);
+
+private:
+	const array<double, 3> center; //!< Box center.
+	const array<double, 3> size; //!< 3D sizes of box.
+	const double granularity; //!< 1D size of grids.
+	vector<vector<size_t>> p_offset; //!< Auxiliary precalculated constants to accelerate grid map creation.
 };
 
 #endif
